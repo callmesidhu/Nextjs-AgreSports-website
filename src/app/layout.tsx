2ae86adfb631aa55e298a440f44aa91c6a34d061
alanjohnck <alanjohnchacko.live@gmail.com>
2025-04-20 20:42:34 +0530

    metdata changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,23 @@ const metalMania = Metal_Mania({
 export const metadata: Metadata = {
   title: "AgreSports - Your Ultimate Sports Hub",
   description: "Stay updated with the latest sports news, videos, and events. AgreSports is your one-stop destination for all things sports.",
-  keywords: ["sports", "videos", "news", "events", "AgreSports"],
+  keywords: [
+    "AgreSports", "agr esports", "agr valorant", "agr valo", "agr gaming",
+    "esports malayalam", "valorant malayalam", "valo malayalam",
+    "best malayalam gaming community", "best esports in kerala",
+    "kerala gaming tournament", "kerala gaming community",
+    "eagle gaming", "best esports community in kerala",
+    "esports in india", "esports india", "esports world cup", "indian esports",
+    "e sports in india", "esports gaming", "e sports game", "esports game", "esports games",
+    "league of legends esports", "lol esports", "lol championship",
+    "esports live", "live esports", "esports lol",
+    "tournament app", "e sports tournament", "esports tournament", "esports tournaments",
+    "what are esports", "what is esports", "esports meaning", "what's e sports",
+    "gaming tournament", "gaming sports", "gaming tournaments",
+    "games tournament", "game tournaments", "video gaming tournaments",
+    "esports full form", "e sports full form",
+    "esport app", "esports tournament app", "gaming competition", "e-sports app"
+  ],
   authors: [{ name: "AgreSports Team", url: "https://agresports.org" }],
   themeColor: "#4A90E2",
   openGraph: {
